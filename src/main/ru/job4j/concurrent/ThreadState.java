93e7b9b778ea931e86bfef873360e7861b4bb4c2
noslav7 <noslav7@gmail.com>
2022-06-09 13:05:51 +0500

    1.1. Состояние нити. [#229175]

--- src/main/ru/job4j/concurrent/ThreadState.java
+++ src/main/ru/job4j/concurrent/ThreadState.java
@@ -18,6 +18,8 @@ public class ThreadState {
         main.start();
         while (first.getState() != Thread.State.TERMINATED ||
                 main.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName());
+            System.out.println(main.getName());
         }
         System.out.println("Работа завершена");
     }
