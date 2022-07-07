f2db24a18eedd3159b1372a45704172f623d57a5
noslav7 <noslav7@gmail.com>
2022-07-07 05:43:59 +0500

    1. Запуск нити. Thread#start() [#1016 #309464]

--- src/main/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/ru/job4j/concurrent/ConcurrentOutput.java
@@ -4,5 +4,6 @@ public class ConcurrentOutput {
     public static void main(String[] args) {
         Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
         second.start();
+        System.out.println(Thread.currentThread().getName());
     }
 }
