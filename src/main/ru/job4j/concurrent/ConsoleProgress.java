e718967bfdb0b25e84ba576e44777775060b4848
noslav7 <noslav7@gmail.com>
2022-07-07 10:48:00 +0500

    3. Прерывание нити [#1019 #311071]

--- src/main/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 public class ConsoleProgress implements Runnable {
 
-    static Thread progress = new Thread(new ConsoleProgress());
-
     public static void main(String[] args) throws InterruptedException {
-        progress.start();
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.run();
         Thread.sleep(4000);
         progress.interrupt();
     }
@@ -16,23 +15,13 @@ public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
-        List<Character> symbols = new ArrayList<>();
-        symbols.add('-');
-        symbols.add('\\');
-        symbols.add('|');
-        symbols.add('/');
-        symbols.add('-');
-        symbols.add('\\');
-        symbols.add('|');
-        symbols.add('/');
+        List<Character> symbols = List.of('-', '\\', '|', '/', '-', '\\', '|', '/');
         for (Character symbol : symbols) {
             System.out.print("\r Loading " + symbol);
-            if (!progress.isInterrupted()) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
