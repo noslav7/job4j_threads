0821035dbb6d862d9f1696cd1e45fa2200d17f76
noslav7 <noslav7@gmail.com>
2022-07-07 16:49:14 +0500

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/ru/job4j/thread/Wget.java
@@ -0,0 +1,52 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String file = this.url;
+        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp2.xml")) {
+            int timeToLoad = 1000;
+            byte[] mockBuffer = new byte[1024];
+            byte[] dataBuffer = new byte[1024];
+            long start = System.currentTimeMillis();
+            int first1024 = in.read(mockBuffer, 0, 1024);
+            long end = System.currentTimeMillis();
+            long elapsedTime = end - start;
+            int waiting = 0;
+            if (elapsedTime < timeToLoad) {
+                waiting = (int) (timeToLoad - elapsedTime);
+            }
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(waiting);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
