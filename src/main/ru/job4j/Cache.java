9f6f094ff8958211d18231f0b36623e7935be80c
noslav7 <noslav7@gmail.com>
2022-07-07 19:28:09 +0500

    1. Синхронизация общих ресурсов. [#1096]

--- src/main/ru/job4j/Cache.java
+++ src/main/ru/job4j/Cache.java
@@ -1,9 +1,12 @@
 package ru.job4j;
 
 public final class Cache {
-    private static Cache cache = new Cache();
+    private static Cache cache;
 
-    public static Cache instOf() {
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
         return cache;
     }
 }
