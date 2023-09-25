91655e2ca8fdfd1d45e26994ef203fc5d1a5e2d1
noslav7 <noslav7@gmail.com>
2023-09-25 18:14:21 +0500

    1. Реализовать шаблон Producer Consumer. [#1098]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -5,20 +5,19 @@ import net.jcip.annotations.ThreadSafe;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
-    private int max_size;
+    private int maxSize;
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
 
-    public SimpleBlockingQueue(int max_size) {
-        this.max_size = max_size;
+    public SimpleBlockingQueue(int maxSize) {
+        this.maxSize = maxSize;
     }
 
     public synchronized void offer(T value) throws InterruptedException {
-        while (queue.size() == max_size) {
+        while (queue.size() == maxSize) {
             wait();
         }
         queue.offer(value);
